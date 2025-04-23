e9d7080a7d613cad5a44ae2c5ab08a85bf7a9b50
barbozapbernardo <clashcomtudo@gmail.com>
2025-04-23 19:33:49 -0300

    jogador x jogador

--- main.py
+++ main.py
@@ -21,27 +21,60 @@ if escolha == 1:
             break
 if escolha == 2:
     print("Você escolheu jogar contra outro jogador!")
+
     while True:
+        print()
         print("Você deseja continuar ou sair?")
         print("1 - Continuar")
         print("0 - Sair")
         continuar = int(input("Digite sua escolha: "))
         if continuar == 1:
-            print("Escolha")
+            escolha_player1 = int(input("Jogador 1 - Escolha qual você deseja: "))
+            escolha_player2 = int(input("Jogador 2 - Escolha qual você deseja: "))
+            
+            opcoes = {1: "Pedra", 2: "Papel", 3: "Tesoura"}
+
+            if escolha_player1 == escolha_player2:
+                print("Empate")
+                print(f"Ambos escolheram {opcoes[escolha_player1]}.")
+            if escolha_player1 == 1 and escolha_player2 == 2:
+                print()
+                print("Jogador 2 ganhou!")
+                print("Papel vence Pedra. Jogador 2 com Papel ganhou de Jogador 1 com Pedra.")
+            if escolha_player1 == 2 and escolha_player2 == 1:
+                print()
+                print("Jogador 1 ganhou!")
+                print("Papel vence Pedra. Jogador 1 com Papel ganhou de Jogador 2 com Pedra.")
+            if escolha_player1 == 2 and escolha_player2 == 3:
+                print()
+                print("Jogador 2 ganhou!")
+                print("Tesoura vence Papel. Jogador 2 com Tesoura ganhou de Jogador 1 com Papel.")
+            if escolha_player1 == 3 and escolha_player2 == 2:
+                print()
+                print("Jogador 1 ganhou!")
+                print("Tesoura vence Papel. Jogador 1 com Tesoura ganhou de Jogador 2 com Papel.")
+            if escolha_player1 == 1 and escolha_player2 == 3:
+                print()
+                print("Jogador 1 ganhou!")
+                print("Pedra vence Tesoura. Jogador 1 com Pedra ganhou de Jogador 2 com Tesoura.")
+            if escolha_player1 == 3 and escolha_player2 == 1:
+                print()
+                print("Jogador 2 ganhou!")
+                print("Pedra vence Tesoura. Jogador 2 com Pedra ganhou de Jogador 1 com Tesoura.")
         else:
             break
-if escolha == 3:
-    print("Você escolheu ver 2 computadores jogando!")
-    while True:
-        print("Você deseja continuar ou sair?")
-        print("1 - Continuar")
-        print("0 - Sair")
-        continuar = int(input("Digite sua escolha: "))
-        if continuar == 1:
-            print("Escolha")
-        else:
-            break
+        if escolha == 3:
+            print("Você escolheu ver 2 computadores jogando!")
+            while True:
+                print("Você deseja continuar ou sair?")
+                print("1 - Continuar")
+                print("0 - Sair")
+                continuar = int(input("Digite sua escolha: "))
+                if continuar == 1:
+                    print("Escolha")
+                else:
+                    break
 print("Obrigado por jogar Jokenpô!")
 
 
-    
+        
