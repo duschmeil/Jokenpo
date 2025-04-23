29d08872ad6997df4f5649c7a528177f2e8bfc5a
eduschmeil <eduschmeil@gmail.com>
2025-04-23 19:50:06 -0300

    arrumar problema
    o continuar tava antes do jogo agora está depois

--- main.py
+++ main.py
@@ -9,14 +9,11 @@ print("2 - Jogar contra outro jogador")
 print("3 - Ver 2 computadores jogando")
 print("0 - Sair")
 escolha = int(input("Digite sua escolha: "))
+continuar = 1
 
 if escolha == 1:
     print("Você escolheu jogar contra o computador!")
     while True:
-        print("Você deseja continuar ou sair?")
-        print("1 - Continuar")
-        print("0 - Sair")
-        continuar = int(input("Digite sua escolha: "))
         if continuar == 1:
             pedra = 1
             papel = 2
@@ -50,15 +47,13 @@ if escolha == 1:
                     print("Empate!")
         else:
             break
+        print("Você quer jogar novamente? ")
+        print("1 - SIM \n2 - NÃO")
+        continuar = int(input("Digiter sua escolha: "))
 if escolha == 2:
     print("Você escolheu jogar contra outro jogador!")
 
     while True:
-        print()
-        print("Você deseja continuar ou sair?")
-        print("1 - Continuar")
-        print("0 - Sair")
-        continuar = int(input("Digite sua escolha: "))
         if continuar == 1:
             escolha_player1 = int(input("Jogador 1 - Escolha qual você deseja: "))
             escolha_player2 = int(input("Jogador 2 - Escolha qual você deseja: "))
@@ -94,13 +89,12 @@ if escolha == 2:
                 print("Pedra vence Tesoura. Jogador 2 com Pedra ganhou de Jogador 1 com Tesoura.")
         else:
             break
+        print("Você quer jogar novamente? ")
+        print("1 - SIM \n2 - NÃO")
+        continuar = int(input("Digiter sua escolha: "))
 if escolha == 3:
     print("Você escolheu ver 2 computadores jogando!")
     while True:
-        print("Você deseja continuar ou sair?")
-        print("1 - Continuar")
-        print("0 - Sair")
-        continuar = int(input("Digite sua escolha: "))
         if continuar == 1:
             pc1 = random.randint(1, 3)
             pc2 = random.randint(1, 3)
@@ -128,6 +122,9 @@ if escolha == 3:
                 sleep(1) 
         else:
             break
+        print("Você quer jogar novamente? ")
+        print("1 - SIM \n2 - NÃO")
+        continuar = int(input("Digiter sua escolha: "))
 print("Obrigado por jogar Jokenpô!")
 
 
